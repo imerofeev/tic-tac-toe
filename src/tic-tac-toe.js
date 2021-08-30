3c1ff52ac809b6cdf2ee61d696282e363ae4ca2c
imerofeev <imerofeev@gmail.com>
2021-08-30 08:02:16 +0300

    feat: tic-tac-toe solution

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,82 @@
 class TicTacToe {
-    constructor() {
+  constructor() {
+    this.currentPlayer = 'x'
+    this.winner = null
+    this.matrix = []
 
+    for (let i = 0; i < 3; i++) {
+      this.matrix[i] = []
+      for (let j = 0; j < 3; j++) {
+        this.matrix[i][j] = null
+      }
+    }
+  }
+
+  getCurrentPlayerSymbol() {
+    return this.currentPlayer
+  }
+
+  nextTurn(rowIndex, columnIndex) {
+    if (this.matrix[rowIndex][columnIndex] === null) {
+      this.matrix[rowIndex][columnIndex] = this.currentPlayer
+      this.currentPlayer = (this.currentPlayer === 'x' ? 'o' : 'x')
+      return true
+    }
+    return false
+  }
+
+  win() {
+    if (this.matrix[0][0] === this.matrix[1][1] && this.matrix[0][0] === this.matrix[2][2]) {
+      return this.matrix[0][0]
     }
 
-    getCurrentPlayerSymbol() {
-
+    if (this.matrix[0][2] === this.matrix[1][1] && this.matrix[0][2] === this.matrix[2][0]) {
+      return this.matrix[0][2]
     }
 
-    nextTurn(rowIndex, columnIndex) {
-
+    for (let i = 0; i < 3; i++) {
+      if (this.matrix[i][0] === this.matrix[i][1] && this.matrix[i][0] === this.matrix[i][2]) {
+        return this.matrix[i][0]
+      }
     }
 
-    isFinished() {
-
+    for (let i = 0; i < 3; i++) {
+      if (this.matrix[0][i] === this.matrix[1][i] && this.matrix[0][i] === this.matrix[2][i]) {
+        return this.matrix[0][i]
+      }
     }
+    return null
+  }
 
-    getWinner() {
+  isFinished() {
+    this.winner = this.win()
+    return !!(this.isDraw() || this.winner !== null);
+  }
 
+  getWinner() {
+    this.winner = this.win()
+    return this.winner
+  }
+
+  noMoreTurns() {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (this.matrix[i][j] === null) {
+          return false
+        }
+      }
     }
+    return true
+  }
 
-    noMoreTurns() {
+  isDraw() {
+    this.winner = this.win()
+    return this.noMoreTurns() && this.winner === null
+  }
 
-    }
-
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+  getFieldValue(rowIndex, colIndex) {
+    return this.matrix[rowIndex][colIndex]
+  }
 }
 
 module.exports = TicTacToe;
